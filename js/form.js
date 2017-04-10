5c008e636abccb9798ca60e2188c602d2f7765ff
Rodrigo Fonseca <rcnweb@gmail.com>
2017-04-10 14:55:34 -0300

    continuacao estudos

--- js/form.js
+++ js/form.js
@@ -4,18 +4,26 @@ botaoAdicionar.addEventListener("click",function(event){
 
 	var form = document.querySelector("#form-adiciona");
 	var paciente = obtemPacienteDoFormulario(form);
-	console.log(paciente);
 	var pacienteTr = montaTr(paciente);
+
+	var erros = validaPaciente(paciente);
+	if(erros.length > 0 ){
+		exibeMensagensDeErro(erros);
+		return;
+	}
+
 	var tabela = document.querySelector("#tabela-pacientes");
 	tabela.appendChild(pacienteTr);
 	form.reset();
+	var mensagensDeErro = document.querySelector("#mensagens-erro");
+	mensagensDeErro.innerHTML = "";
 });
 
 function obtemPacienteDoFormulario(form){
 	var paciente = {
 		nome : form.nome.value,
 		peso : form.peso.value,
-		altura : form.peso.value,
+		altura : form.altura.value,
 		gordura : form.gordura.value,
 		imc : calculaimc(form.peso.value,form.altura.value)
 	}
@@ -40,4 +48,25 @@ function montaTd(dado,classe){
 	td.textContent = dado;
 	td.classList.add(classe);
 	return td;	
+}
+
+function validaPaciente(paciente){
+	var erros = [];
+	if(paciente.nome.length == 0) erros.push("O nome não pode ser em branco.");
+	if(paciente.peso.length == 0) erros.push("O peso não pode ser em branco.");
+	if(paciente.altura.length == 0) erros.push("A altura não pode ser em branco.");
+	if(paciente.gordura.length == 0) erros.push("A gordura não pode ser em branco.");
+	if(!validaPeso(paciente.peso)) erros.push("Peso é inválido");
+	if(!validaAltura(paciente.altura)) erros.push("Altura é inválida");
+	return erros;
+}
+
+function exibeMensagensDeErro(erros){
+	var mensagensErro = document.querySelector("#mensagens-erro");
+	mensagensErro.innerHTML = "";
+	erros.forEach(function(erro){
+		var li = document.createElement("li");
+		li.textContent = erro;
+		mensagensErro.appendChild(li);
+	});
 }
