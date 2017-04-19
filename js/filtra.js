28d276a5251b572ed961fdbbc9e60983c64e5100
Rodrigo Fonseca <rcnweb@gmail.com>
2017-04-18 23:20:01 -0300

    keep

--- js/filtra.js
+++ js/filtra.js
@@ -1,10 +1,21 @@
 var campoFiltro = document.querySelector("#filtrar-tabela");
 campoFiltro.addEventListener("input",function(){
 	var pacientes = document.querySelectorAll(".paciente");
-	for(var i=0; i<pacientes.length;i++){
+	if(this.value.length > 0 ){
+		for(var i=0; i<pacientes.length;i++){
 		var paciente = pacientes[i];
 		var tdNome = paciente.querySelector(".info-nome");
 		var nome = tdNome.textContent;
-		console.log(this.value);
+		if(nome != this.value){
+			paciente.classList.add("invisivel");
+		}else{
+			paciente.classList.remove("invisivel");
+		}
+		}	
+	}else{
+		for (var i = 0; i < pacientes.length; i++) {
+            var paciente = pacientes[i];
+            paciente.classList.remove("invisivel");
+        }
 	}
 });
