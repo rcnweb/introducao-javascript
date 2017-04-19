213ab0089842b16eac8f7c52d52afacc519f4e37
Rodrigo Fonseca <rcnweb@gmail.com>
2017-04-19 16:12:26 -0300

    cap 8

--- js/filtra.js
+++ js/filtra.js
@@ -3,14 +3,15 @@ campoFiltro.addEventListener("input",function(){
 	var pacientes = document.querySelectorAll(".paciente");
 	if(this.value.length > 0 ){
 		for(var i=0; i<pacientes.length;i++){
-		var paciente = pacientes[i];
-		var tdNome = paciente.querySelector(".info-nome");
-		var nome = tdNome.textContent;
-		if(nome != this.value){
-			paciente.classList.add("invisivel");
-		}else{
-			paciente.classList.remove("invisivel");
-		}
+			var paciente = pacientes[i];
+			var tdNome = paciente.querySelector(".info-nome");
+			var nome = tdNome.textContent;
+			var expressao = RegExp(this.value,"i");
+			if(!expressao.test(nome)){
+				paciente.classList.add("invisivel");
+			}else{
+				paciente.classList.remove("invisivel");
+			}
 		}	
 	}else{
 		for (var i = 0; i < pacientes.length; i++) {
